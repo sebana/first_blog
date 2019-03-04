2b46225cb3a3d3d202b1d88b3e6bec84c010ad63
sebana <38051464+sebana@users.noreply.github.com>
2019-03-05 07:09:18 +0900

    third commit

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -20,12 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'lblz3ygl)8)-*jbh@q)8@mju=oa6(%5_-lj8i)s%z86q2pu@$g'
+# SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
 import os
-ECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg## SECURITY WARNING: keep the secret key used in production secret!
-    # SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
-    import os
-    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
